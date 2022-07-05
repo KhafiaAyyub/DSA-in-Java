4c0bb4716be75ad91593891dac49b18ca239d93a
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2022-07-05 23:45:55 +0530

    commit

---
+++ BinaryTrees/TiltOfBT.java
@@ -0,0 +1,16 @@
+static int tilt = 0;
+  public static int tilt(Node node){
+    if(node == null){
+      return 0;
+    }
+    //will return left's sum and change tilt for left side
+    int ls = tilt(node.left);
+    int rs = tilt(node.right);
+    
+    int ltilt = Math.abs(ls-rs);
+    tilt += ltilt;
+
+    int ts = ls + rs + node.data;
+    return ts;
+
+  }
